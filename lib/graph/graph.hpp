c3d20764859f531491f2f73cd6e8ef6de79f34ab
Kacper Kafara <kacperkafara@gmail.com>
2020-07-06 18:51:54 +0200

    Pliki dla klasy graph

--- lib/graph/graph.hpp
+++ lib/graph/graph.hpp
@@ -1,1 +1,23 @@
 // K. Kafara
+
+#ifndef __GRAPH_HPP__
+#define __GRAPH_HPP__
+
+
+#include  "./../linkedlist/linkedlist.hpp"
+
+/**
+ * Klasa reprezentująca graf w postaci listy sąsiedztwa
+ */
+template<class V, class E> class Graph
+{
+private:
+    int rank;
+
+    
+public:
+    Graph(/* args */);
+    ~Graph();
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -9,17 +9,7 @@ int main()
 {
     LOG_MESSAGE();
 
-    LinkedList<int> ll;
 
-    for (int i = 0; i < 10; ++i)
-    {
-        ll.PushBack(i);
-    }
 
-    ll.print();
-
-    ll.PushFront(11);
-
-    ll.print();
     return 0;
 }
