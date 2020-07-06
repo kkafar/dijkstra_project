1aa1e9c54871c2847be808934c8d5b1ae333ded1
Kacper Kafara <kacperkafara@gmail.com>
2020-07-06 16:31:59 +0200

    Linked list, first commit, git branch test, 2

--- lib/linkedlist/linkedlist.cpp
+++ lib/linkedlist/linkedlist.cpp
@@ -1,1 +1,34 @@
 // K. Kafara
+
+#include "linkedlist.hpp"
+
+template<class T> LinkedList<T>::Node * LinkedList<T>::NewNode(const T data, const Node * next, const Node * prev) const
+{
+    Node * node = new Node;
+    node->data = data;
+    node->next = next;
+    node->prev = prev;
+    return Node;
+}
+
+
+template<class T> LinkedList<T>::LinkedList()
+{
+    tail = NewNode((T)(0), nullptr, nullptr);
+    head = NewNode((T)(0), tail, nullptr);
+}
+
+template<class T> LinkedList<T>::~LinkedList()
+{
+    Node * tmp = head->next;
+
+    delete head;
+
+    while (tmp != nullptr)
+    {
+        head = tmp;
+        tmp = tmp->next;
+
+        delete head;
+    }
+}
--- lib/linkedlist/linkedlist.hpp
+++ lib/linkedlist/linkedlist.hpp
@@ -1,1 +1,35 @@
 // K. Kafara
+
+#ifndef __LINKEDLIST_HPP__
+#define __LINKEDLIST_HPP__
+
+/**
+ * Struktura danych mająca zastąpić std::vector.
+ * Realizowana poprzez listę odsyłaczową.  
+ */
+template<class T> class LinkedList
+{
+private:
+    Node * head, * tail;
+
+public:
+    struct Node
+    {
+        T data;
+        Node * next, * prev;
+    };
+    
+
+    LinkedList();
+    
+    ~LinkedList();
+
+    void PushBack(const T data);
+
+    Node * NewNode(const T data, const Node * next, const Node * prev) const;
+
+
+
+};
+
+#endif
