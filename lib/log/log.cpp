6b48a3c7d41cbe497dd13f30ee345930ea8d2410
Kacper Kafara <kacperkafara@gmail.com>
2020-07-10 23:58:52 +0200

    Zmiana głównego systemu log'ów
    Funkcję wyświetlającą komunikaty umieściłem w obrębie klasy,
    dodając możliwość sterowania jakie wiadomości mają być wyświetlane.
    Dodano także typ wyliczeniowy opisujący typ danego komunikatu.

--- lib/log/log.cpp
+++ lib/log/log.cpp
@@ -13,42 +13,56 @@ void LOG_MESSAGE(const std::string & message, const std::string & FILE, const in
     std::cout << FILE << ": " << LINE << ": " << message << "\n";
 }
 ////////////////////////////////////////////////////////////
-Log::Log(const int level) : level(level), info("info: "), warn("warning: "), error("error: ")
+// int Log::global_level = 2;
+////////////////////////////////////////////////////////////
+Log::Log(const int level) : local_level(level)
 {
-    std::cout << __FILE__ << ": " << __LINE__ << ": " << "Konstruktor klasy `Log`\n";
+    // std::cout << __FILE__ << ": " << __LINE__ << ": " << "Konstruktor klasy `Log`\n";
+    Message(__FILE__, __LINE__, INFO, 2, "Konstruktor klasy `Log`");
 }
 ////////////////////////////////////////////////////////////
 Log::~Log()
 {
-    std::cout << __FILE__ << ": " << __LINE__ << ": " << "Destruktor klasy `Log`\n";
+    // std::cout << __FILE__ << ": " << __LINE__ << ": " << "Destruktor klasy `Log`\n";
+    Message(__FILE__, __LINE__, INFO, 2, "Destruktor klasy `Log`");
 }
 ////////////////////////////////////////////////////////////
-void Log::SetLevel(const int level)
+/* void Log::SetLevel(const int level)
 {
-    Log::level = level;
-}
+    local_level = level;
+} */
 ////////////////////////////////////////////////////////////
-void Log::SetMessage(const std::string & msg)
+/* void Log::SetMessage(const std::string & msg)
 {
     message = msg;
-}
+} */
 ////////////////////////////////////////////////////////////
 void Log::LogMessage(const std::string & msg, const int lvl, const std::string & FILE, const int & LINE) const
 {
-    if (lvl <= level)
+    if (lvl <= local_level)
     {
         std::cout << FILE << ": " << LINE << ": ";
 
         if (lvl == 0)
-            std::cout << info;
+            std::cout << "INFO: ";
 
         else if (lvl == 1)
-            std::cout << warn;
+            std::cout << "WARN: ";
 
         else 
-            std::cout << error;
+            std::cout << "ERROR: ";
 
         std::cout << msg << "\n";
     }
+}
+////////////////////////////////////////////////////////////
+void TEST_BEG(const std::string& test_name)
+{
+    std::cout << "\n==================== " << test_name << " ====================\n";
+}
+////////////////////////////////////////////////////////////
+void TEST_END()
+{
+    std::cout << "============================================================\n";
 }
 ////////////////////////////////////////////////////////////
--- lib/log/log.hpp
+++ lib/log/log.hpp
@@ -16,6 +16,23 @@ void LOG_MESSAGE(const std::string & message, const std::string & FILE, const in
 ////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////
+// @brief Wyświetla ramkę testową (1/2)
+// @param test_name - nazwa testowanej funkcjonalności (wyświetlana w konsoli)
+////////////////////////////////////////////////////////////
+void TEST_BEG(const std::string& test_name = "Unknown");
+////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////
+// @brief Wyświetla ramkę testową (2/2)
+////////////////////////////////////////////////////////////
+void TEST_END();
+////////////////////////////////////////////////////////////
+
+
+
+
+
+////////////////////////////////////////////////////////////
 // Wyświetla podane w parametrach informacje (wraz z miejscem wywołania tej funkcji)
 ////////////////////////////////////////////////////////////
 template<class... Args> void LogMessage(const std::string & FILE, const int LINE, Args... args)
@@ -34,27 +51,106 @@ private:
     // 1 - info oraz warning
     // 2 - info, warning, error
     ////////////////////////////////////////////////////////////
-    int level;
-    const std::string info;
-    const std::string warn;
-    const std::string error;
-    std::string message;
-
+    
+    int local_level;
+    // bool active;
+    // const std::string info;
+    // const std::string warn;
+    // const std::string error;
+    // std::string message;
 public:
+    inline static int global_level = 2;
+    enum MessageType {INFO = 0, WARNING = 1, ERROR = 2, OTHER = 3};
     Log(const int level = 0);
     ~Log();
 
     ////////////////////////////////////////////////////////////
-    /// @brief Ustawia maksymalny `poziom` wyświetlanych wiadomości 
+    // @brief Ustawia maksymalny `poziom` wyświetlanych wiadomości 
+    // Nadaj wyższy poziom jeżeli nie chcesz danej wiadomośći. 
     ////////////////////////////////////////////////////////////
-    inline void SetLevel(const int level);
+    inline void SetLevel(const int level)
+    {
+        local_level = level;
+    }
+    ////////////////////////////////////////////////////////////
+
+
 
     ////////////////////////////////////////////////////////////
-    // 
+    // inline void SetMessage(const std::string & msg);
     ////////////////////////////////////////////////////////////
-    inline void SetMessage(const std::string & msg);
+
+    ////////////////////////////////////////////////////////////
+    // @brief TODO
+    ////////////////////////////////////////////////////////////
     ////////////////////////////////////////////////////////////
     void LogMessage(const std::string & msg, const int lvl, const std::string & FILE, const int & LINE) const;
+
+
+
+    ////////////////////////////////////////////////////////////
+    // Wyświetla podane w parametrach informacje (wraz z miejscem wywołania tej funkcji)
+    ////////////////////////////////////////////////////////////
+    template<class... Args> static void GlobalMessage(const std::string & FILE, const int LINE, MessageType type, const int lvl, Args... args)
+    {
+        if (lvl <= global_level)
+        {
+            std::cout << FILE << ": " << LINE << ": ";
+            switch (type)
+            {
+                case INFO:
+                    std::cout << "INFO: ";
+                    break;
+
+                case WARNING:
+                    std::cout << "WARN: ";
+                    break;
+
+                case ERROR: 
+                    std::cout << "ERROR: ";
+                    break;
+                
+                case OTHER:
+                default:
+                    break;
+            }
+            (std::cout << ... << args);
+            std::cout << "\n";
+        }
+    }
+    ////////////////////////////////////////////////////////////
+
+    ////////////////////////////////////////////////////////////
+    // Wyświetla podane w parametrach informacje (wraz z miejscem wywołania tej funkcji)
+    ////////////////////////////////////////////////////////////
+    template<class... Args> void Message(const std::string & FILE, const int LINE, MessageType type, const int lvl, Args... args)
+    {
+        if (lvl <= local_level)
+        {
+            std::cout << FILE << ": " << LINE << ": ";
+            switch (type)
+            {
+                case INFO:
+                    std::cout << "INFO: ";
+                    break;
+
+                case WARNING:
+                    std::cout << "WARN: ";
+                    break;
+
+                case ERROR: 
+                    std::cout << "ERROR: ";
+                    break;
+                
+                case OTHER:
+                default:
+                    break;
+            }
+            (std::cout << ... << args);
+            std::cout << "\n";
+        }
+    }
+    ////////////////////////////////////////////////////////////
 };
 
 
